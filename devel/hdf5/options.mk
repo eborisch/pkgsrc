3076b25babe61f57a02dc75d8e4da08132b8acac
Eric A. Borisch - Builder <eborisch@gmail.com>
2018-10-24 16:51:15 -0500

    Added my options to hdf5 (allow threads and c++).

--- devel/hdf5/options.mk
+++ devel/hdf5/options.mk
@@ -1,7 +1,7 @@
 # $NetBSD: options.mk,v 1.8 2019/05/01 22:39:29 bacon Exp $
 
 PKG_OPTIONS_VAR=PKG_OPTIONS.hdf5
-PKG_SUPPORTED_OPTIONS=	szip threads
+PKG_SUPPORTED_OPTIONS=	szip threads unsupported eab
 PKG_SUGGESTED_OPTIONS=	szip
 
 .include "../../mk/bsd.options.mk"
@@ -15,4 +15,18 @@ CONFIGURE_ARGS+=	--with-szlib
 CONFIGURE_ARGS+=	--enable-threadsafe
 # XXX libraries should not be linked against -lpthread
 .include "../../mk/pthread.buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Munsupported)
+CONFIGURE_ARGS+=	--enable-unsupported
+.endif
+
+PLIST_VARS+=		eab
+
+.if !empty(PKG_OPTIONS:Meab)
+USE_LANGUAGES=		c c++
+CONFIGURE_ARGS+=	--enable-cxx
+CONFIGURE_ARGS+=	--with-default-plugin-dir=${PREFIX}/lib/hdf5
+INSTALLATION_DIRS+=     lib/hdf5
+PLIST.eab=		yes
 .endif
